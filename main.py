cdf11e70baf129b08a8588a6121fd4e15e52d693
Edison Moreland <edison@devdude.net>
2017-11-01 16:22:56 -0700

    improved example and move to python 3

--- main.py
+++ main.py
@@ -1,49 +1,42 @@
 # coding=utf-8
-# !/usr/bin/python
+# terrible example agent from wiki
 
 import socket
 import struct
 import sexpdata
+HOST = 'localhost'
+PORT = 3100
 
 
 def send_effector(msg):
-    '''Each message is prefixed with the length of the payload message.
-    The length prefix is a 32 bit unsigned integer in network order'''
-    print("S:", msg)
-    sock.send(struct.pack("!I", len(msg)) + msg.encode("ascii"))
+    """
+    Each message is prefixed with the length of the payload message.
+    The length prefix is a 32 bit unsigned integer in network order
+    """
+    msg = msg.encode("ascii")
+    sock.send(struct.pack("!I", len(msg)) + msg)
 
 
 def recieve_perceptors():
+    # get header
     length_no = sock.recv(4)
     length = struct.unpack("!I", length_no)
-    sexprstr = sock.recv(length[0])
-    sexprlist = sexpdata.dumps(sexprstr)
-    perceptors = {}
-    for subl in sexprlist:
-        if ('HJ' in subl):
-            perceptors[subl[1][1]] = float(subl[2][1])
 
-    print("R: hj1", perceptors["hj1"], "\n")
+    # get rest of data
+    sexprstr = sock.recv(length[0])
+    perceptors = sexpdata.dumps(sexprstr)
+
     return perceptors
 
 
-HOST = 'localhost'
-PORT = 3100
+print("connecting to server")
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((HOST, PORT))
 
+print("initialising")
 send_effector('(scene rsg/agent/nao/nao.rsg)')
-recieve_perceptors()
+print(sock.recv(1024))
 send_effector('(init (unum 0)(teamname NaoRobot))')
-recieve_perceptors()
+print(sock.recv(1024))
 send_effector('(he1 1)')
-perceptors = recieve_perceptors()
-
-while True:
-    if (perceptors["hj1"] > 120.0):
-        send_effector('(he1 -1)')
-
-    if (perceptors["hj1"] < -120):
-        send_effector('(he1 1)')
-
-    perceptors = recieve_perceptors()
+print(sock.recv(1024))

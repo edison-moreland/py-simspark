b446cfe454c5ca1fa3e0cbdf29e62fd9b62aa2e2
Edison Moreland <edison@devdude.net>
2017-11-03 08:30:35 -0700

    Redid connecting to server, added TODO's for logging

--- py-simspark/simspark_server.py
+++ py-simspark/simspark_server.py
@@ -1,21 +1,55 @@
 import socket
 import struct
 import sys
+import logging
+
+
+class SimSparkServerError(BaseException):
+    pass
+
+
+class NoResponseError(SimSparkServerError):
+    pass
 
 
 class SimSparkServer(object):
-    def __init__(self, host: str, port: int):
-        self.host = host
-        self.port = port
+    def __init__(self, host: str, port: int, timeout: int=30, retries: int=5):
+        """
+        Intermediate class for interacting with SimSpark
+        Args:
+            host: IP Adresss of the SimSpark server
+            port: TCP port of the SimSpark server
+            timeout: Time out time in seconds
+            retries: Number of times connection can timeout before the connection is closed
+        """
+        self.address = (host, port)
+        self.retries = retries
 
+        # Initialize socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.settimeout(timeout=timeout)
 
     def connect(self):
         """Connect socket to SimSpark server"""
-        self.sock.connect((self.host, self.port))  # TODO: This can be so much better
+        error = None
+        for _ in range(self.retries):  # Keep connecting until successful, or number of retries reached
+            try:
+                # TODO(LOGGING) log connecting to server
+                self.sock.connect(self.address)
+
+                break  # Connection successful, break loop
+
+            except socket.timeout as er:
+                # TODO(LOGGING) log retries
+                error = er  # Save error to re-raise it later
+                continue
+        else:
+            # TODO(LOGGING) Log failed connection
+            raise error
 
     def disconnect(self):
         """Disconnect socket from SimSpark server"""
+        # TODO(LOGGING) Log disconnect from server
         self.sock.close()
 
     def send_message(self, msg: str):
@@ -29,10 +63,12 @@ class SimSparkServer(object):
         payload = msg.encode("ascii")
 
         # Messages are prefixed with length of payload as a 32 bit unsigned integer
-        prefix = struct.pack("!I", len(payload))
+        prefix = struct.pack("!I", len(payload))  # TODO(ERROR) This should have some error-catching
 
         # Send message to server
         self.sock.send(prefix + payload)
+
+        # TODO(LOGGING) Log message sent to server
 
     def receive_message(self):
         """
@@ -41,16 +77,18 @@ class SimSparkServer(object):
         Returns:
             str
         """
-        # get header
+        # Messages are prefixed with length of payload as a 32 bit unsigned integer
         prefix_raw = self.sock.recv(4)
         try:
             payload_length = struct.unpack("!I", prefix_raw)
+
         except struct.error:
-            sys.exit("No data received from server, it probably stopped. Exiting")
+            # TODO(LOGGING) Log this error
+            raise NoResponseError("No data received from server, it probably stopped. Exiting")
 
         # get rest of data
         raw_payload = self.sock.recv(payload_length[0])
-        assert isinstance(raw_payload, bytes)
         payload = raw_payload.decode("ascii")
 
+        # TODO(LOGGING) Log message sent to server
         return payload
